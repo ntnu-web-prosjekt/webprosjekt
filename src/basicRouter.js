5bddec563a69e71d7443840298666f67363af9de
Magnus <69847549+Magnumoe@users.noreply.github.com>
2022-03-10 09:50:39 +0100

    added Routing, not working yet

--- src/basicRouter.js
+++ src/basicRouter.js
@@ -1,4 +1,72 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-export default function basicRouting() {}
+export default function basicRouting() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/myRequests">My Requests</Link>
+          </li>
+          <li>
+            <Link to="/inbox">Inbox</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link to="/settings">Settings</Link>
+          </li>
+          <li>
+            <Link to="/UserProfile">UserProfile</Link>
+          </li>
+          <li>
+            <Link to="/findUser">Find User</Link>
+          </li>
+          <li>
+            <Link to="/registration">Registrations</Link>
+          </li>
+          <li>
+            <Link to="/sidebar">Sidebar</Link>
+          </li>
+          <li>
+            <Link to="/catalogue">catalogue</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Routes>
+          <Route exact path="/" element={<Dashboard />}></Route>
+
+          <Route exact path="/login" element={<Login />}></Route>
+
+          <Route exact path="/myRequsts" element={<MyRequsts />}></Route>
+
+          <Route exact path="/inbox" element={<Inbox />}></Route>
+
+          <Route exact path="/profile" element={<Profile />}></Route>
+
+          <Route exact path="/settings" element={<Settings />}></Route>
+
+          <Route exact path="/userProfile" element={<UserProfile />}></Route>
+
+          <Route exact path="/findUser" element={<FindUser />}></Route>
+
+          <Route exact path="/registration" element={<Registration />}></Route>
+
+          <Route exact path="/sidebar" element={<Sidebar />}></Route>
+
+          <Route exact path="/catalogue" element={<Catalogue />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
