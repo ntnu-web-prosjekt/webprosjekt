53f520ddaa0982092c03c407cf5b4c64c73e0a7e
Magnus <69847549+Magnumoe@users.noreply.github.com>
2022-03-16 13:27:10 +0100

    Profileinfo, tags & aboutme html/css

--- src/components/profile/ProfileInfo.jsx
+++ src/components/profile/ProfileInfo.jsx
@@ -5,10 +5,13 @@ export default class ProfileInfo extends Component {
     return (
       <div>
         <h2>My Profile</h2>
-        <div>
-          <img src="http://placehold.it/" alt="placeholder" />
+        <div className="profile-info">
+          <img src="https://via.placeholder.com/200" alt="placeholder" />
           <h3>John Wayne</h3>
           <span>Associate Professor</span>
+          <p>NTNU Gjøvik</p>
+          <a href="tel:+4733378901">+47 333 78 901</a>
+          <a href="mailto:someone@example.com">Send email</a>
         </div>
       </div>
     );
--- src/components/profile/Tags.jsx
+++ src/components/profile/Tags.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class Tags extends Component {
   render() {
     return (
-      <div>
+      <div className="tags-container">
         <h2>My tags</h2>
         <ul>
           <li>Javascript</li>
@@ -11,6 +11,9 @@ export default class Tags extends Component {
           <li>Vue</li>
           <li>Angular</li>
           <li>HTML</li>
+          <li>Javascript</li>
+          <li>React</li>
+          <li>Vue</li>
         </ul>
       </div>
     );
