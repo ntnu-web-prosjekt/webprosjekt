6b47aa5f146bfc0cca4092b858016646c2aa5a08
Henrik Granum <henriklg@stud.ntnu.no>
2022-03-16 22:15:49 +0100

    V1 of Sidebar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./sass/main.scss";
-import App from "./components/About";
+import App from "./components/Sidebar";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
