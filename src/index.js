9b5b4c10a96b259af1064feee24dcc9c14d6207e
Sivert <sivertgh@stud.ntnu.no>
2022-03-17 10:00:14 +0100

    Setup dashboard

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./sass/main.scss";
-import App from "./components/Sidebar";
+import App from "./components/Dashboard";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
