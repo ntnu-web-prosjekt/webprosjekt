3149677b8794cbec2a14b32d64f7f0c53ab87ee4
Magnus <69847549+Magnumoe@users.noreply.github.com>
2022-03-17 11:05:29 +0100

    myreview & pendigreq

--- src/components/profile/MyReview.jsx
+++ src/components/profile/MyReview.jsx
@@ -3,8 +3,15 @@ import React, { Component } from "react";
 export default class MyReview extends Component {
   render() {
     return (
-      <div>
+      <div className="myReview">
         <h2>My Review</h2>
+        <textarea
+          placeholder="Review of teacher..."
+          id="myReview"
+          rows="20"
+          cols="60"
+        ></textarea>
+        <button type="button">Edit</button>
       </div>
     );
   }
--- src/components/profile/PendingReq.jsx
+++ src/components/profile/PendingReq.jsx
@@ -3,8 +3,22 @@ import React, { Component } from "react";
 export default class PendingReq extends Component {
   render() {
     return (
-      <div>
+      <div className="pendingReq-container">
         <h2>Pending Requests</h2>
+        <ul>
+          <li>
+            Advanced CSS{" "}
+            <button type="button" className="viewReqBtn">
+              view
+            </button>
+          </li>
+          <li>
+            Fullstack{" "}
+            <button type="button" className="viewReqBtn">
+              view
+            </button>
+          </li>
+        </ul>
       </div>
     );
   }
