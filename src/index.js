b5a43b05097da386f76751f4b1a13ee7e533782a
Sivert <sivertgh@stud.ntnu.no>
2022-03-22 10:04:04 +0100

    router setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./sass/main.scss";
-import App from "./components/Catalogue";
+import App from "./basicRouter";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(<App />, document.getElementById("root"));
