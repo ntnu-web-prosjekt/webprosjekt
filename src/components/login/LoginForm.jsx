7bf628e902353ad303e8bc656204b785d43a796e
sivertgullberghansen <sivertgh@stud.ntnu.no>
2022-03-24 10:10:26 +0100

    fixed login styling

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,27 +1,37 @@
-import React from 'react';
-import exammatcher_logo from 'C:/Users/Sivan/OneDrive/Dokumenter/Webprosjekt/src/exammatcher_logo.png';
+import React from "react";
 
 class LoginForm extends React.Component {
-    render() {
-        return (
-            <div class="loginform">
-                <img src={exammatcher_logo} alt="Logo" id='logo' />
-                <form>
-                    <label>
-                        <p>E-mail</p>
-                        <input type="text" name="email" id="email" class="inputField" />
-                    </label>
-                    <label>
-                        <p>Password</p>
-                        <input type="text" name="password" id="password" class="inputField"/>
-                    </label>
-                    <input type="submit" name="login" id="login" value="Sign in" />
-                </form>
-                <a href='test' id='forgot'>Forgot password?</a>
-                <button class="request">Request user account</button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div class="loginform">
+        <img
+          src={process.env.PUBLIC_URL + "img/logo.svg"}
+          alt="Logo"
+          id="logo"
+        />
+        <form>
+          <label>
+            <p>E-mail</p>
+            <input type="text" name="email" id="email" class="inputField" />
+          </label>
+          <label>
+            <p>Password</p>
+            <input
+              type="text"
+              name="password"
+              id="password"
+              class="inputField"
+            />
+          </label>
+          <input type="submit" name="login" id="login" value="Sign in" />
+        </form>
+        <a href="test" id="forgot">
+          Forgot password?
+        </a>
+        <button class="request">Request user account</button>
+      </div>
+    );
+  }
 }
 
 export default LoginForm;
