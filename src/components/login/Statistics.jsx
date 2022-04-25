911cc11579c510b257519e630e378ac065b989f4
Henrik Granum <henriklg@stud.ntnu.no>
2022-04-25 12:25:09 +0200

    connected stats to backend, removed forgot password, added height to 'request user account'

--- src/components/login/Statistics.jsx
+++ src/components/login/Statistics.jsx
@@ -2,6 +2,35 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 class Statistics extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      openRequests: null,
+      matchedRequests: null
+    }
+
+    this.retrieveStats = this.retrieveStats.bind(this);
+  }
+
+  componentDidMount(){
+    this.retrieveStats();
+  }
+
+  // Retrieves the open and matched requests from back-end and updates the state
+  retrieveStats() {
+    const url = process.env.REACT_APP_API_URL + "/stats/view";
+    
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          openRequests: data.openReq,
+          matchedRequests: data.matchedReq
+        });
+      });
+  }
+
   render() {
     return (
       <div id="statistics">
@@ -14,11 +43,11 @@ class Statistics extends React.Component {
         <div id="statbox-container">
           <div className="statbox">
             <p className="stattype">Open requests</p>
-            <p className="statvalue">155</p>
+            <p className="statvalue">{this.state.openRequests != null ? this.state.openRequests : "X"}</p>
           </div>
           <div className="statbox">
             <p className="stattype">Matched requests</p>
-            <p className="statvalue">89</p>
+            <p className="statvalue">{this.state.matchedRequests != null ? this.state.matchedRequests : "X"}</p>
           </div>
         </div>
       </div>
