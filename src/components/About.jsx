0dc91ee2aba59c7be1e4beae3e5f3dcc9c64cea8
Henrik Granum <henriklg@stud.ntnu.no>
2022-04-25 12:36:15 +0200

    Added text to about the project, and paragraph width

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,32 +3,10 @@ import "./about/about.scss";
 function About() {
   return (
     <div className="about">
-      <h1>About this website</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-        vehicula sem eget elit vestibulum, nec mollis augue laoreet. Aenean ac
-        varius augue, a porttitor mi. Proin tincidunt dolor ut vulputate
-        bibendum. Duis sagittis magna et est iaculis, finibus sollicitudin
-        lectus euismod. Proin molestie id leo nec iaculis. Praesent eget dictum
-        leo, a hendrerit turpis. Curabitur porttitor maximus odio, et aliquet
-        erat convallis nec. Curabitur vitae odio sollicitudin, commodo odio
-        cursus, varius dui. Fusce ac tempus metus. Vestibulum commodo tincidunt
-        tellus, non dignissim metus. Mauris a eros viverra, fringilla metus
-        vitae, feugiat enim. Vestibulum a consequat nisi. Cras dolor justo,
-        accumsan et dapibus ut, rutrum et diam. Orci varius natoque penatibus et
-        magnis dis parturient montes, nascetur ridiculus mus. Quisque aliquet
-        hendrerit lacinia. Fusce suscipit augue ut laoreet bibendum.
-      </p>
-      <p>
-        Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc
-        commodo sem fringilla felis aliquam, nec dignissim dolor lacinia. Donec
-        malesuada quam nisl, eu bibendum ex pretium sit amet. Phasellus auctor
-        ipsum eget nibh vulputate suscipit. In eu erat facilisis, placerat
-        mauris id, porta ligula. Nam tincidunt elementum diam, et ullamcorper
-        lacus volutpat sed. Sed nibh sem, sollicitudin vel iaculis in, efficitur
-        a nulla. Quisque ac turpis et velit porta scelerisque. Nam blandit justo
-        in ligula blandit aliquam. Praesent volutpat urna eu sapien rhoncus
-        pellentesque.
+      <h1>About ExamMatcher</h1>
+      <p className="para">
+        ExamMatcher makes it easy to get a second examiner for your exam. Once you have published a request, others will be able to apply for your request until you have chosen a second examiner.
+        With this software you can also have a look trough a catalogue to find requests you want to apply for.  
       </p>
       <h2>Meet the creators</h2>
       <p className="creators">
