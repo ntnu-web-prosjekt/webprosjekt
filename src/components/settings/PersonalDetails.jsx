8b356688195032f70a017db58dacecd1f1dfc225
Magnus <69847549+Magnumoe@users.noreply.github.com>
2022-04-30 22:09:24 +0200

    started on settings Personal details form

--- src/components/settings/PersonalDetails.jsx
+++ src/components/settings/PersonalDetails.jsx
@@ -1,43 +1,41 @@
 import React, { Component } from "react";
 // import Tags from "../profile/Tags";
 import "../profile/profile.scss";
+const API_HOST = process.env.REACT_APP_API_URL;
 export default class PersonalDetails extends Component {
   render() {
     return (
       <div className="Personal-details">
         <h2>Personal Details</h2>
-        <label>First name</label>
-        <input type="text" />
-        <br />
-        <label>Last name</label>
-        <input type="text" />
-        <br />
-        <label>Title</label>
-        <select>
-          <option value="test">Assistant Professor</option>
-          <option value="test">Associate Professor</option>
-          <option value="test">Professor</option>
-          <option value="test">Instructor</option>
-          <option value="test">Professor Emeritus</option>
-        </select>
-        <br />
-        <label>University</label>
-        <select>
-          <option value="test">NTNU Gjøvik</option>
-          <option value="test">NTNU Ålesund</option>
-          <option value="test">NTNU Trondheim</option>
-          <option value="test">UiO</option>
-          <option value="test">NMBU</option>
-          <option value="test">Høgskolen i Innlandet</option>
-        </select>
-        <br />
-        <label>Phone Number</label>
-        <input type="tel" />
-        <br />
-        <label>Email</label>
-        <input type="email" />
-
-        {/* <Tags /> */}
+        <form
+          action={API_HOST + "/settings/update/data"}
+          autoComplete="off"
+          onSubmit={this.handleSubmit}
+        >
+          <label>First name</label>
+          <input type="text" />
+          <br />
+          <label>Last name</label>
+          <input type="text" />
+          <br />
+          <label>Description</label>
+          <textarea
+            className="description"
+            name="description"
+            rows="4"
+            cols="60"
+            onChange={this.handleChange}
+            placeholder="Type here..."
+          />
+          <br />
+          <label>Phone Number</label>
+          <input type="tel" />
+          <br />
+          <label>Email</label>
+          <input type="email" />
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
