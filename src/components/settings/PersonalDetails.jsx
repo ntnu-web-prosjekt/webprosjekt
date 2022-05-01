2f6f37d32fa2ff28d947e10c5150872cc6ff700c
Magnus <69847549+Magnumoe@users.noreply.github.com>
2022-05-01 15:41:41 +0200

    fixed update details form, 404 error server

--- src/components/settings/PersonalDetails.jsx
+++ src/components/settings/PersonalDetails.jsx
@@ -1,42 +1,120 @@
-import React, { Component } from "react";
-// import Tags from "../profile/Tags";
-import "../profile/profile.scss";
-const API_HOST = process.env.REACT_APP_API_URL;
-export default class PersonalDetails extends Component {
-  render() {
-    return (
-      <div className="Personal-details">
-        <h2>Personal Details</h2>
-        <form
-          action={API_HOST + "/settings/update/data"}
-          autoComplete="off"
-          onSubmit={this.handleSubmit}
-        >
-          <label>First name</label>
-          <input type="text" />
-          <br />
-          <label>Last name</label>
-          <input type="text" />
-          <br />
-          <label>Description</label>
-          <textarea
-            className="description"
-            name="description"
-            rows="4"
-            cols="60"
-            onChange={this.handleChange}
-            placeholder="Type here..."
-          />
-          <br />
-          <label>Phone Number</label>
-          <input type="tel" />
-          <br />
-          <label>Email</label>
-          <input type="email" />
-          <br />
-          <input type="submit" value="Submit" />
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function PersonalDetails() {
+  const redirect = useNavigate();
+  const [request, setRequest] = useState({});
+
+  // useEffect(() => {
+  //   const req = `${API_HOST}/settings`;
+  //   const fetchRequest = async () => {
+  //     try {
+  //       const response = await fetch(req, {
+  //         method: "POST",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //         body: JSON.stringify(localStorage.getItem("token")),
+  //       });
+  //       const data = await response.json();
+  //       console.log(data);
+  //       setRequest(data);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+  //   fetchRequest();
+  // }, [params._id]);
+
+  const updateRequest = (event) => {
+    const API_HOST = process.env.REACT_APP_API_URL;
+    const req = `${API_HOST}/settings/update/data`;
+    console.log(request);
+    const fetchRequest = async () => {
+      try {
+        const response = await fetch(req, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(request),
+        });
+        const data = await response.json();
+        console.log(data);
+        redirect("/profile");
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchRequest();
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setRequest((request) => ({
+      ...request,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    updateRequest();
+  };
+
+  return (
+    <div>
+      <h1>
+        <span>Personal Details</span>
+      </h1>
+      <div className="form-group">
+        <form autoComplete="off" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="firstName">First Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="firstName"
+              name="firstName"
+              value={request.firstName}
+              onChange={handleChange}
+            />
+
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="lastName"
+              name="lastName"
+              value={request.lastName}
+              onChange={handleChange}
+            />
+
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={request.email}
+              onChange={handleChange}
+            />
+
+            <label htmlFor="phone">Phone</label>
+            <input
+              type="tel"
+              className="form-control"
+              id="phone"
+              name="phone"
+              value={request.phone}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Update
+          </button>
         </form>
       </div>
-    );
-  }
+    </div>
+  );
 }
