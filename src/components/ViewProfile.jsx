c6c9bc0bce41ca3b856f2ebcf53663798e646de1
Magnus <69847549+Magnumoe@users.noreply.github.com>
2022-05-03 13:58:05 +0200

    fixed reviews and svg colors

--- src/components/ViewProfile.jsx
+++ src/components/ViewProfile.jsx
@@ -13,8 +13,9 @@ export default class ViewProfile extends Component {
     this.state = {
       user: {},
       id: window.location.pathname.split("/")[2],
+      userId: JSON.parse(sessionStorage.getItem("token"))._id,
       notes: null,
-      loadingNote: true
+      loadingNote: true,
     };
   }
 
@@ -28,59 +29,66 @@ export default class ViewProfile extends Component {
         console.log(this.state.user);
       });
 
-      await fetch(
-        `${process.env.REACT_APP_API_URL}/finduser/review/${JSON.parse(sessionStorage.getItem("token"))._id}`
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          const user = this.state.id;
+    await fetch(
+      `${process.env.REACT_APP_API_URL}/finduser/review/${
+        JSON.parse(sessionStorage.getItem("token"))._id
+      }`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const user = this.state.id;
 
-          if(data.notes[user]){
+        if (data.notes[user]) {
           const note = data.notes[user];
-          this.setState({notes: note, loadingNote: false});
-          
-          } else {
-            this.setState({loadingNote: false});
-          }
-        });
+          this.setState({ notes: note, loadingNote: false });
+        } else {
+          this.setState({ loadingNote: false });
+        }
+      });
   }
 
   updateReview = (newText) => {
-    this.setState({notes: newText}, () => {
+    this.setState({ notes: newText }, () => {
       const body = {
         userId: this.state.id,
         ownerId: JSON.parse(sessionStorage.getItem("token"))._id,
-        reviewText: this.state.notes
-      }
-  
+        reviewText: this.state.notes,
+      };
+
       console.log(body);
-  
-      fetch(
-        `${process.env.REACT_APP_API_URL}/finduser/review/update`, {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(body)
-        }).then((response) => response.json())
+
+      fetch(`${process.env.REACT_APP_API_URL}/finduser/review/update`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      })
+        .then((response) => response.json())
         .then((data) => {
-          alert("The review is now saved")
+          alert("The review is now saved");
         });
     });
-  }
+  };
 
   render() {
-    if (this.state.user.name && this.state.loadingNote == false ) {
+    if (this.state.user.name && this.state.loadingNote === false) {
       return (
         <div className="page">
-            <Sidebar />
+          <Sidebar />
           <div className="pageContent Profile">
             <ProfileInfo {...this.state.user} />
             <div>
-            <Tags {...this.state.user} />
-            <AboutMe {...this.state.user} />
-            <PendingReq {...this.state.user} />
-            <MyReview {...this.state.user} note={this.state.notes} onClick={this.updateReview}/>
+              <Tags {...this.state.user} />
+              <AboutMe {...this.state.user} />
+              <PendingReq {...this.state.user} />
+              {this.state.id !== this.state.userId && (
+                <MyReview
+                  {...this.state.user}
+                  note={this.state.notes}
+                  onClick={this.updateReview}
+                />
+              )}
             </div>
           </div>
         </div>
